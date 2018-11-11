39de3cd495e8486f003c3da9d464914ff97c91aa
zhongciting2009 <43277827+zhongciting2009@users.noreply.github.com>
2018-11-11 23:12:32 +0800

    Update EndScene.java

--- EndScene.java
+++ EndScene.java
@@ -4,20 +4,22 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 
 /**
- * ½áÊø³¡¾°
+ * ç»“æŸåœºæ™¯
  */
 public class EndScene extends JPanel  implements Runnable{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	ImageIcon end = new ImageIcon(TankWar.class.getResource("end.jpg"));
 	private int x;
 	private int y;
-	private int width;
+	//private int width;
 	private int height;
 	private int times=0;
 	private boolean threadAlive=true;
@@ -25,7 +27,7 @@ public class EndScene extends JPanel  implements Runnable{
 		super();
 		this.x = x;
 		this.y = y;
-		this.width = width;
+		//this.width = width;
 		this.height = height;
 	}
 	
@@ -44,36 +46,35 @@ public class EndScene extends JPanel  implements Runnable{
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
-		g.setColor(Color.BLACK);
-		g.fillRect(x, y, width, height);
+		g.drawImage(end.getImage(),x,y,700,height,null); 
 		int sceneNumber=MyTankData.sceneNumber;
 		int totalScore=MyTankData.totalScore;
 		times++;
 		if(times%3==0){
 			g.setColor(Color.GREEN);
-			Font font=new Font("»ªÎÄĞĞ¿¬",Font.BOLD,40) ;
+			Font font=new Font("åæ–‡è¡Œæ¥·",Font.BOLD,40) ;
 			g.setFont(font);
-			g.drawString("Game Over!!", 100, 140);
-			g.drawString("Äúµ½´ï¹ØÊı£º"+sceneNumber+" ¹Ø",100,220);
-			g.drawString("×Ü   ·Ö£º"+totalScore+"·Ö",100,300);
+			g.drawString("Game Over!!", 150, 140);
+			g.drawString("æ‚¨åˆ°è¾¾å…³æ•°ï¼š"+sceneNumber+" å…³",150,220);
+			g.drawString("æ€»   åˆ†ï¼š"+totalScore+"åˆ†",150,300);
 			 
 		}else if(times%3==1){
 			g.setColor(Color.cyan);
-			Font font=new Font("»ªÎÄĞĞ¿¬",Font.BOLD,40) ;
+			Font font=new Font("åæ–‡è¡Œæ¥·",Font.BOLD,40) ;
 			g.setFont(font);
-			g.drawString("Game Over!!", 100, 140);
-			g.drawString("Äúµ½´ï¹ØÊı£º"+sceneNumber+" ¹Ø",100,220);
-			g.drawString("×Ü   ·Ö£º"+totalScore+"·Ö",100,300);
+			g.drawString("Game Over!!", 150, 140);
+			g.drawString("æ‚¨åˆ°è¾¾å…³æ•°ï¼š"+sceneNumber+" å…³",150,220);
+			g.drawString("æ€»   åˆ†ï¼š"+totalScore+"åˆ†",150,300);
 		}else{
 			g.setColor(Color.pink);
-			Font font=new Font("»ªÎÄĞĞ¿¬",Font.BOLD,40) ;
+			Font font=new Font("åæ–‡è¡Œæ¥·",Font.BOLD,40) ;
 			g.setFont(font);
-			g.drawString("Game Over!!", 100, 140);
-			g.drawString("Äúµ½´ï¹ØÊı£º"+sceneNumber+" ¹Ø",100,220);
-			g.drawString("×Ü   ·Ö£º"+totalScore+"·Ö",100,300);
+			g.drawString("Game Over!!", 150, 140);
+			g.drawString("æ‚¨åˆ°è¾¾å…³æ•°ï¼š"+sceneNumber+" å…³",150,220);
+			g.drawString("æ€»   åˆ†ï¼š"+totalScore+"åˆ†",150,300);
 		}
 		g.setColor(Color.white);
-		g.setFont(new Font("»ªÎÄĞĞ¿¬",Font.BOLD,25));
+		g.setFont(new Font("åæ–‡è¡Œæ¥·",Font.BOLD,25));
 		
 	}
 
